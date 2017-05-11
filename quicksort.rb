d289ff27cbdd22a30a8c5678c67a22fb66828bf8
jacobmoretti <me@jacobmoretti.com>
2017-05-10 21:20:42 -0700

    Added comment

--- quicksort.rb
+++ quicksort.rb
@@ -4,6 +4,7 @@ def quicksort(array)
   if array.size < 2
     array
   else
+    # TODO: this pivot should be randomized in some manner...
     pivot = array[0]
     less = array.drop(1).select { |i| i <= pivot }
     greater = array.drop(1).select { |i| i > pivot }
